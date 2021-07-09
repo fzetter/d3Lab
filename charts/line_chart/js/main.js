55cba20a53b1240e915e8099996361f3bed9d14e
Fer <zetter@Fers-MacBook-Air.local>
2021-07-09 18:12:14 -0500

    line chart

--- charts/line_chart/js/main.js
+++ charts/line_chart/js/main.js
@@ -1,114 +1,153 @@
-/*
-*    main.js
-*/
+// Nests
+// *****
+let yields = [
+	{yield: 27.00, variety: "Manchuria", year: 1931, site: "University Farm"},
+	{yield: 48.87, variety: "Manchuria", year: 1931, site: "Waseca"},
+	{yield: 27.43, variety: "Manchuria", year: 1931, site: "Morris"},
+]
 
-var margin = { left:80, right:100, top:50, bottom:100 },
-    height = 500 - margin.top - margin.bottom, 
-    width = 800 - margin.left - margin.right;
+let byYear = d3.nest().key(d => d.year).entries(yields)
+let byYearVariety = d3.nest().key(d => d.year).key(d => d.variety).entries(yields)
 
-var svg = d3.select("#chart-area").append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom);
+console.log(byYear)
+console.log(byYearVariety)
 
-var g = svg.append("g")
-    .attr("transform", "translate(" + margin.left + 
-        ", " + margin.top + ")");
+// Config
+// ******
+const margin = {left: 80, right: 100, top: 50, bottom: 100}
+const width = 800, height = 400
 
-// Time parser for x-scale
-var parseTime = d3.timeParse("%Y");
-// For tooltip
-var bisectDate = d3.bisector((d) => { return d.year; }).left;
+const g = d3.select("#chart-area")
+	.append("svg")
+		.attr("width", width + margin.right + margin.left)
+		.attr("height", height + margin.top + margin.bottom)
+	.append("g")
+		.attr("transform", "translate(" + margin.left + ", " + margin.top + ")")
 
 // Scales
-var x = d3.scaleTime().range([0, width]);
-var y = d3.scaleLinear().range([height, 0]);
+const x = d3.scaleTime().range([0, width])
+const y = d3.scaleLinear().range([0, height])
 
-// Axis generators
-var xAxisCall = d3.axisBottom()
-var yAxisCall = d3.axisLeft()
-    .ticks(6)
-    .tickFormat((d) => { return parseInt(d / 1000) + "k"; });
+// Axis
+const xAxis = d3.axisBottom(x)
+const yAxis = d3.axisLeft(y).ticks(4).tickFormat(d => parseInt(d / 1000) + "k")
+const y_axis = g.append("g").attr("class", "y axis")
+const x_axis = g.append("g").attr("class", "x axis").attr("transform", "translate(0, " + height + ")")
 
-// Axis groups
-var xAxis = g.append("g")
-    .attr("class", "x axis")
-    .attr("transform", "translate(0," + height + ")");
-var yAxis = g.append("g")
-    .attr("class", "y axis")
-    
-// Y-Axis label
-yAxis.append("text")
-    .attr("class", "axis-title")
-    .attr("transform", "rotate(-90)")
-    .attr("y", 6)
-    .attr("dy", ".71em")
-    .style("text-anchor", "end")
-    .attr("fill", "#5D6971")
-    .text("Population)");
+// Time Format
+const parseDate = d3.timeParse("%Y")
+const formatDate = d3.timeFormat("%Y")
+const bisectDate = d3.bisector(d => d.year).left
 
-// Line path generator
-// TODO: Implement the line generator
+// Labels
+y_axis.append("text")
+	.attr("class", "axis-title")
+  .attr("transform", "rotate(-90)")
+  .attr("y", 6)
+  .attr("dy", ".71em")
+  .style("text-anchor", "end")
+  .attr("fill", "#5D6971")
+  .text("Population)")
 
-d3.json("data/example.json").then((data) => {
-    // Data cleaning
-    data.forEach((d) => {
-        d.year = parseTime(d.year);
-        d.value = +d.value;
-    });
+// DATA
+// *****
+d3.json("https://raw.githubusercontent.com/gcastillo56/d3Lab/master/charts/line_chart/data/example.json").then(data => {
+    parse(data)
 
-    // Set scale domains
-    // TODO: set domain of axes
+    // Scale Domains
+    const y_data = "value", x_data = "year"
 
-    // Generate axes once scales have been set
-    xAxis.call(xAxisCall.scale(x))
-    yAxis.call(yAxisCall.scale(y))
+    const x_ranges = d3.extent(data, d => d[x_data])
+		const y_max = d3.max(data, d => d[y_data])
+		x.domain(x_ranges)
+    y.domain([y_max, 765*1000])
 
-    // Add line to chart
-    // TODO: add line path
+		// Line Chart
+		const line = d3.line()
+		  .x(d => x(d[x_data]))
+		  .y(d => y(d[y_data]))
 
-    /******************************** Tooltip Code ********************************/
+		g.append("path")
+		  .attr("class", "line")
+		  .attr("fill", "none")
+		  .attr("stroke", "grey")
+		  .attr("stroke-with", "3px")
+		  .attr("d", line(data))
 
-    var focus = g.append("g")
-        .attr("class", "focus")
-        .style("display", "none");
+    // Axis & Labels
+		x_axis.call(xAxis)
+		y_axis.call(yAxis)
 
-    focus.append("line")
-        .attr("class", "x-hover-line hover-line")
-        .attr("y1", 0)
-        .attr("y2", height);
+		// Focus
+		const focus = g.append("g")
+				.attr("class", "focus")
+				.style("display", "none")
 
-    focus.append("line")
-        .attr("class", "y-hover-line hover-line")
-        .attr("x1", 0)
-        .attr("x2", width);
+		// Append x line
+		focus.append("line")
+				.attr("class", "x-hover-line hover-line")
+				.attr("y1", 0)
+				.attr("y2", height)
+				.style("opacity", 0.7)
 
-    focus.append("circle")
-        .attr("r", 7.5);
+		// Append y line
+		focus.append("line")
+				.attr("class", "y-hover-line hover-line")
+				.attr("x1", 0)
+				.attr("x2", width)
+				.style("opacity", 0.7)
 
-    focus.append("text")
-        .attr("x", 15)
-        .attr("dy", ".31em");
+	  // Append circle at intersection
+		focus.append("circle")
+				.attr("r", 7.5)
 
-    g.append("rect")
-        .attr("class", "overlay")
-        .attr("width", width)
-        .attr("height", height)
-        .on("mouseover", () => { focus.style("display", null); })
-        .on("mouseout", () => { focus.style("display", "none"); })
-        .on("mousemove", mousemove);
+	  // Append y-data at intersection
+		focus.append("text")
+			  .attr("class", "txt1")
+				.attr("x", 15)
+				.attr("dy", ".31em")
 
-    function mousemove() {
-        var x0 = x.invert(d3.mouse(this)[0]),
-            i = bisectDate(data, x0, 1),
-            d0 = data[i - 1],
-            d1 = data[i],
-            d = x0 - d0.year > d1.year - x0 ? d1 : d0;
-        focus.attr("transform", "translate(" + x(d.year) + "," + y(d.value) + ")");
-        focus.select("text").text(d.value);
-        focus.select(".x-hover-line").attr("y2", height - y(d.value));
-        focus.select(".y-hover-line").attr("x2", -x(d.year));
-    }
-    /******************************** Tooltip Code ********************************/
+		// Append x-data at intersection
+		focus.append("text")
+			  .attr("class", "txt2")
+				.attr("x", 15)
+				.attr("dy", "1.5em")
 
-});
+		// Capture mouse
+		g.append("rect")
+				.attr("class", "overlay")
+				.attr("width", width)
+				.attr("height", height)
+				.on("mouseover", () => { focus.style("display", null) })
+				.on("mouseout", () => { focus.style("display", "none") })
+				.on("mousemove", mousemove)
 
+		function mousemove() {
+				let x0 = x.invert(d3.mouse(this)[0])
+				let i = bisectDate(data, x0, 1)
+				let d0 = data[i - 1]
+				let d1 = data[i]
+				let d = x0 - d0[x_data] > d1[x_data] - x0 ? d1 : d0
+				focus.attr("transform", "translate(" + x(d[x_data]) + "," + y(d[y_data]) + ")")
+				focus.select("text.txt1").text(d3.format(",.2r")(d[y_data]))
+				focus.select("text.txt2").text(formatDate(d[x_data]))
+				focus.select(".x-hover-line").attr("y2", height - y(d[y_data]))
+				focus.select(".y-hover-line").attr("x2", -x(d[x_data]))
+		}
+
+})
+
+// Parse Integers
+// **************
+function parse(data) {
+  data.forEach(item => {
+    Object.keys(item).forEach(curr => {
+      if (!isNaN(item[curr])) item[curr] = parseInt(item[curr])
+			if (curr === "year") item[curr] = parseDate(item[curr])
+    })
+  })
+  return data
+}
+
+// NOTES:
+// https://github.com/d3/d3-scale-chromatic
